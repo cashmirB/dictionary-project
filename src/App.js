65137650732453bf7d5af665e6b53160f4f6d815
cashmirB <128428924+cashmirB@users.noreply.github.com>
2023-09-12 00:36:20 +0100

    added basic styling on h1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Dictionary App</h1>
+        <a href="#" className="btn btn-success">
+          Hello
+        </a>
       </header>
     </div>
   );
 }
-
-export default App;
